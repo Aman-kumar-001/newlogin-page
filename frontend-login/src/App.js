5904cbe8a2ec9b8c6a8333481a752c0c0bd4764f
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-03-10 14:03:41 +0530

    ok

--- frontend-login/src/App.js
+++ frontend-login/src/App.js
@@ -1,35 +1,30 @@
-import { useState } from 'react';
-import Home from './component/home/home';
-import Login from './component/login/login';
-import Register from './component/register/register';
-import { BrowserRouter , Routes , Route } from 'react-router-dom';
-
+import { useState } from "react";
+import Home from "./component/home/home";
+import Login from "./component/login/login";
+import Register from "./component/register/register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  
-
-  const [data , Setdata] = useState(false);
+  const [data, Setdata] = useState(false);
 
   const handleLogin = (isLoggedIn) => {
     Setdata(isLoggedIn);
   };
 
   const handleLogout = () => {
-    Setdata(false); 
+    Setdata(false);
   };
-   
 
   return (
     <BrowserRouter>
-     
-    <Routes>
-      <Route path='/' element={ <Home data={data}  onLogout={handleLogout}/>}></Route>
-      <Route path='/login' element={ <Login ondata={handleLogin}/>}></Route>
-      <Route path='/register' element={ <Register/>}></Route>
-    
-    
-    
-    </Routes>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home data={data} onLogout={handleLogout} />}
+        ></Route>
+        <Route path="/login" element={<Login ondata={handleLogin} />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
